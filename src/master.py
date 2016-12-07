caa644e23aae368f8bb1e415f0bcb56970e0dec5
akeenan2 <ann.keenan2@gmail.com>
2016-12-07 16:57:40 -0500

    add files

--- src/master.py
+++ src/master.py
@@ -3,16 +3,36 @@ import time
 from collections import Counter
 import db
 
-while True:
-	masterDict = Counter()
+def usage(status=0):
+    print '''Usage: ./master.py [options]...
+
+Options:
+	-f 			use files
+    -h          help'''
+    sys.exit(status)
+
+def master():
+	master_dict = Counter()
+
+	with open(FILE,'r+') as f:
+        # build a dictionary from the file
+        for line in f:
+            l = line.split(' ')
+            WORDS[l[0]] = l[1].rstrip()
+        f.close()
+
 	# urls 
 	for url in db.urls:
-		dict = Counter(mapper(url))
-		masterDict = masterDict + dict
+		temp_dict = Counter(mapper(url))
+		master_dict = master_dict + temp_dict
 		
-	masterDict = reduce(masterDict)
-	masterDict = sort(masterDict)
-	
+	master_dict = reducer(master_dict)
+	master_dict = sort(master_dict)
 	
 	time.sleep(10)
 
+# main execution
+if __name__ == '__main__':
+
+	while True:
+		master()
---
+++ src/reducer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2.7
+import sys
+import getopt
+from mapper import *
+import db # from db.py
+
+def reduce():
+    
