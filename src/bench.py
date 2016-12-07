3492fc7c28081bb50d157fac5ddd3309317db840
Catherine <cbadart@hotmail.com>
2016-12-07 15:52:34 -0500

    trying to benchmark performance of sorting algorithms

---
+++ src/bench.py
@@ -0,0 +1,17 @@
+#bench.py
+#runs the different sorts on the results of mapper.py and times each one
+
+import os
+import sys
+import getopt
+from mapper import *
+import db # from db.py
+
+#defaults
+SORT = ['quick', 'merge']
+
+# main execution
+if __name__ == '__main__':
+    for s in SORT:
+    	command = "time python sort.py -f ../data/wsj-map.txt -s " + s
+    	os.system(command)
