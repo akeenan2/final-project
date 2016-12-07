9912f2ac0d89f502f204e68fe0e729f4647c3204
Catherine <cbadart@hotmail.com>
2016-12-07 17:19:48 -0500

    added sort options to master.py

--- src/master.py
+++ src/master.py
@@ -2,11 +2,13 @@
 import time
 from collections import Counter
 import db
+import getopt
 
 def usage(status=0):
     print '''Usage: ./master.py [options]...
 
 Options:
+	-s SORT 	type of sort to use
     -h          help'''
     sys.exit(status)
 
@@ -18,22 +20,25 @@ def master():
 		master_dict = master_dict + temp_dict
 		
 	master_dict = reducer(master_dict)
-	master_dict = sort(master_dict)
+	master_dict = sort(master_dict, SORT)
 	
 	time.sleep(10)
 #defaults
-
+SORT = 'default'
 
 # main execution
 if __name__ == '__main__':
 	#parse options
 	try:
-        opts,args = getopt.getopt(sys.argv[1:], "h")
+        opts,args = getopt.getopt(sys.argv[1:], "s:h")
     except getopt.GetoptError as err:
         print err
         usage()
 
     for o,a in opts:
-    	usage(1)
+    	if o == '-s':
+    		SORT = a
+    	else:
+    		usage(1)
 	
 	master()
