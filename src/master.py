08790d4987acb619311e6469a3fc2ee6e6bc82de
Catherine <cbadart@hotmail.com>
2016-12-07 17:10:14 -0500

    got rid of file stuff in master.py

--- src/master.py
+++ src/master.py
@@ -7,48 +7,33 @@ def usage(status=0):
     print '''Usage: ./master.py [options]...
 
 Options:
-	-f 			use files
     -h          help'''
     sys.exit(status)
 
-def master(use_file):
+def master():
 	master_dict = Counter()
-	if use_file == 1:
-		with open(FILE,'r+') as f:
-	        # build a dictionary from the file
-	        for line in f:
-	            l = line.split(' ')
-	            WORDS[l[0]] = l[1].rstrip()
-	        f.close()
-
-	else:
-		# urls 
-		for url in db.urls:
-			temp_dict = Counter(mapper(url))
-			master_dict = master_dict + temp_dict
-			
-		master_dict = reducer(master_dict)
-		master_dict = sort(master_dict)
+	# urls 
+	for url in db.urls:
+		temp_dict = Counter(mapper(url))
+		master_dict = master_dict + temp_dict
+		
+	master_dict = reducer(master_dict)
+	master_dict = sort(master_dict)
 	
 	time.sleep(10)
 #defaults
-USE_FILE = 0
-FILE = ''
+
 
 # main execution
 if __name__ == '__main__':
 	#parse options
 	try:
-        opts,args = getopt.getopt(sys.argv[1:], "f:h")
+        opts,args = getopt.getopt(sys.argv[1:], "h")
     except getopt.GetoptError as err:
         print err
         usage()
 
     for o,a in opts:
-       if o == '-f':
-            USE_FILE = 1
-            FILE = a
-        else:
-            usage(1)
+    	usage(1)
 	
-	master(USE_FILE)
+	master()
