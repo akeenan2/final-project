08a394a1d443f32a7cabeba31d6ca602bd7ebdf6
akeenan2 <ann.keenan2@gmail.com>
2016-12-09 15:09:57 -0500

    implement a search for top headlines against the headlines found in the defined news article urls

--- src/mapper.py
+++ src/mapper.py
@@ -5,7 +5,11 @@ import requests
 from string import punctuation
 import unicodedata
 import getopt
+import re
 import db
+
+# store the headlines/url for each news url
+HEADLINES = dict()
 
 # url - url of website being scraped
 # headline_paths - a list of xpaths to scrape all the headlines
@@ -50,19 +54,54 @@ def sanitize(headline):
     return words
 
 # url - url of website being scraped
-# key_words - map of key words to their frequency
+# keywords - map of key words to their frequency
 def mapper(url):
+    global HEADLINES
     headline_paths = db.url_map[url].split(';')[0].split('|')
     url_paths = db.url_map[url].split(';')[1].split('|')
     articles = scraper(url,headline_paths,url_paths)
+    HEADLINES[url] = articles
     # store the key words mapped to a count of the number of times repeated
-    key_words = dict()
+    keywords = dict()
     # iterate through the headlines
     for headline,url in articles.iteritems():
+        # if url is not valid, ignore
+        if url == "javascript:void(0);":
+            continue
         words = sanitize(headline)
         for word in words:
-            if word in key_words:
-                key_words[word] += 1
+            if word in keywords:
+                keywords[word] += 1
             else:
-                key_words[word] = 1
-    return key_words
+                keywords[word] = 1
+    return keywords
+
+# url - search for headlines
+# keywords - tuple list of keywords with their count
+def lookup(search_url,keywords):
+    global HEADLINES
+#    headline_paths = db.url_map[search_url].split(';')[0].split('|')
+#    url_paths = db.url_map[search_url].split(';')[1].split('|')
+#    articles = scraper(search_url,headline_paths,url_paths)
+    # store the list of headlines that have at least one match
+    headlines = []
+    for headline,url in HEADLINES[search_url].iteritems():
+        # if url is not valid, ignore
+        if url == "javascript:void(0);":
+            continue
+        words = sanitize(headline)
+        weight = 0 # weight of the matches
+        matches = 0 # number of word matches
+        for word in words:
+            for key_word in keywords:
+                # check for substrings
+                if word in key_word[0] or key_word[0] in word:
+                    weight += key_word[1]
+                    matches += 1
+        # only append if there was a match and the url is not null
+        if matches > 0:
+            if not re.match("^https?://.*",url):
+                # append the url to the search url if a relative path
+                url = search_url[:-1] + url
+            headlines.append((matches,weight,headline,url))
+    return headlines
--- src/master.py
+++ src/master.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python2.7
 import sys
 import time
-from collections import Counter
-import db
-from mapper import mapper
-from sort import sort
-import getopt
 from lxml import html
 import requests
+from collections import Counter
+import getopt
+
+import db
+from mapper import mapper,lookup
+from sort import sort
 
 def usage(status=0):
     print '''Usage: ./master.py [options]...
 
 Options:
+    -n NUM      use NUM keywords in search (default 5)
+    -l NUM      output NUM links (default 2)
+
+    -g          google search on keywords (default custom-built search)
+
     -o          output all keywords found with their counts
-    -n NUM      use NUM keywords in search
-    -d          disable the search for headlines
     -t          print the top words with their counts
+    -d          disable the search for headlines
+
     -h          help'''
     sys.exit(status)
 
@@ -25,67 +31,92 @@ def search(keywords):
     for word in keywords[:-1]:
         url = url + word[0] + '+'
     url = url + keywords[-1][0]
-    # fetch the website
+
+    # fetch the search result page
     page = requests.get(url)
     tree = html.fromstring(page.content)
 
-    headlines = []
-    links = []
-
-    headline = tree.xpath("//h2[contains(@class,'esc-lead-article-title')]/a/span/text()")
-    link = tree.xpath("//h2[contains(@class,'esc-lead-article-title')]/a/@href")
-    for i in range(10):
-        print headline[i]
-        print link[i]
+    # get the google search output
+    headlines = tree.xpath("//h2[contains(@class,'esc-lead-article-title')]/a/span/text()")
+    links = tree.xpath("//h2[contains(@class,'esc-lead-article-title')]/a/@href")
+    
+    # return the list of headlines
+    return zip(headlines,links)
 
 def master(urls):
-    master = Counter()
+    keywords = Counter()
     # call mapper on all urls and reduce to one master dict
     for url in urls:
         temp_dict = Counter(mapper(url))
-        master = master + temp_dict
-    # sort by count
-    master = sort(master)
-    return master
+        keywords = keywords + temp_dict
+    # sort by count and return
+    return sort(keywords)
 
+NUM_KEYWORDS = 5
+NUM_LINKS = 2
+GOOGLE = False
 OUTPUT = False
-NUM = 5
-DISABLE = False
 TOP = False
+DISABLE = False
 
 # main execution
 if __name__ == '__main__':
     # parse options
     try:
-        opts,args = getopt.getopt(sys.argv[1:], "on:dth")
+        opts,args = getopt.getopt(sys.argv[1:], "n:l:gotdh")
     except getopt.GetoptError as err:
         print err
         usage()
 
     for o,a in opts:
-        if o == '-o':
+        if o == '-n':
+            NUM_KEYWORDS = int(a)
+        elif o == '-l':
+            NUM_LINKS = int(a)
+        elif o =='-g':
+            GOOGLE = True
+        elif o == '-o':
             OUTPUT = True
-        elif o == '-n':
-            NUM = int(a)
-        elif o == '-d':
-            DISABLE = True
         elif o == '-t':
             TOP = True
+        elif o == '-d':
+            DISABLE = True
         else:
             usage(1)
 
     # call master function
     words = master(db.urls)
 
-    # print results
+    # print results (all keywords + counts)
     if OUTPUT:
         for word in words:
             print word[0] + ' ' + str(word[1])
 
     # pull out most frequent words
-    trending = words[:-1*(NUM+1):-1]
     if TOP:
-        for word in trending:
+        for word in words[:-1*(NUM_KEYWORDS+1):-1]:
             print word[0] + ' ' + str(word[1])
+
     if not DISABLE:
-        search(trending)
+        # use google search
+        if GOOGLE:
+            headlines = search(words[:-1*(NUM_KEYWORDS+1):-1])
+            # print the top headlines
+            for headline in headlines[:NUM_LINKS]:
+                print headline[0]
+                print headline[1]
+                print '\n',
+        # use our own search/weighting algorithm
+        else:
+            headlines = []
+            # compile list of all headlines from sources with matches to keywords
+            for url in db.urls:
+                # call the algorithm (in mapper.py)
+                headlines += lookup(url,words[:-1*(NUM_KEYWORDS+1):-1])
+            # sort by matches and then by weight
+            headlines.sort(key=lambda element: (element[0], element[1]))
+            # print the top headlines
+            for headline in headlines[:-1*(NUM_LINKS+1):-1]:
+                print headline[2]
+                print headline[3]
+                print '\n',
